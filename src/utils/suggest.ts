1157f3fc01be4e918170264719dd19ee6403e357
Carlos ZAbaleta <zabaletacopa@gmail.com>
2022-06-17 23:00:41 -0400

    add /suggest

---
+++ src/utils/suggest.ts
@@ -0,0 +1,40 @@
+import { restaurants } from '../constants/data.json';
+
+const getRandomRestaurant = () => {
+  return restaurants[Math.floor(Math.random() * restaurants.length)];
+}
+
+const restaurantToBlocks = (restaurant: any) => {
+  return {
+    type: 'section',
+    text: {
+      type: 'mrkdwn',
+      text: `Suggesting: *${restaurant.name}* ${restaurant.id}`,
+    }
+  }
+}
+
+const newSuggestionButtonBlock = {
+    type: 'button',
+    text: {
+      type: 'plain_text',
+      text: 'New suggestion',
+      emoji: true,
+    },
+    value: 'click_me_123',
+    action_id: 'new-suggestion',
+  }
+
+
+const getSuggestion = (prevSuggestionIds?: Array<number>) => {
+  const restaurant = getRandomRestaurant();
+
+  return {
+    blocks: [
+      restaurantToBlocks(restaurant),
+      { type: 'actions', elements: [newSuggestionButtonBlock] }
+    ]
+  }
+}
+
+export { getSuggestion }
