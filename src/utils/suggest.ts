91046bc08efc7578e3c6b31fe16e5c098c44f557
Jax Feist <j@jacksonfeist.com>
2022-06-18 00:15:31 -0400

    added more sheez

--- src/utils/suggest.ts
+++ src/utils/suggest.ts
@@ -1,18 +1,8 @@
-<<<<<<< HEAD
-import restaurantsJson from '../constants/data.json';
-
-const suggestNew = () => {
-  const index = Math.floor(Math.random() * Object.keys(restaurantsJson.restaurants).length + 1);
-  return restaurantsJson.restaurants[index];
-};
-
-export { suggestNew };
-=======
 import { restaurants } from '../constants/data.json';
 
 const getRandomRestaurant = () => {
   return restaurants[Math.floor(Math.random() * restaurants.length)];
-}
+};
 
 const restaurantToBlocks = (restaurant: any) => {
   return {
@@ -20,32 +10,27 @@ const restaurantToBlocks = (restaurant: any) => {
     text: {
       type: 'mrkdwn',
       text: `Suggesting: *${restaurant.name}* ${restaurant.id}`,
-    }
-  }
-}
+    },
+  };
+};
 
 const newSuggestionButtonBlock = {
-    type: 'button',
-    text: {
-      type: 'plain_text',
-      text: 'New suggestion',
-      emoji: true,
-    },
-    value: 'click_me_123',
-    action_id: 'new-suggestion',
-  }
-
+  type: 'button',
+  text: {
+    type: 'plain_text',
+    text: 'New suggestion',
+    emoji: true,
+  },
+  value: 'click_me_123',
+  action_id: 'new-suggestion',
+};
 
 const getSuggestion = (prevSuggestionIds?: Array<number>) => {
   const restaurant = getRandomRestaurant();
 
   return {
-    blocks: [
-      restaurantToBlocks(restaurant),
-      { type: 'actions', elements: [newSuggestionButtonBlock] }
-    ]
-  }
-}
+    blocks: [restaurantToBlocks(restaurant), { type: 'actions', elements: [newSuggestionButtonBlock] }],
+  };
+};
 
-export { getSuggestion }
->>>>>>> ae210838ff6c3e37f4c156c2796a88033aec0345
+export { getSuggestion };
